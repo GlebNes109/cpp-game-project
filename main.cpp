2e3348bcd03fe745f473d4ff110910534ef72cfc
GlebNes109 <testnesyutin@gmail.com>
2025-03-21 03:46:43 +0300

    поменял цвета))

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main() {
     initscr(); // для ncurses
     start_color(); // Включить поддержку цветов
     
-    init_pair(1, COLOR_RED, COLOR_RED);
+    init_pair(1, COLOR_WHITE, COLOR_RED);
     init_pair(2, COLOR_GREEN, COLOR_BLACK);
     init_pair(3, COLOR_RED, COLOR_BLACK);
     init_pair(4, COLOR_YELLOW, COLOR_BLACK);
